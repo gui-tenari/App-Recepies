c0d7d39ca50893a6125cc36e61eed73177400076
Gui-Tenari <guilherme.tenarie@gmail.com>
2021-09-22 15:39:32 -0300

    Corrigindo erros do Lint

--- src/pages/Login.js/index.js
+++ src/pages/Login.js/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 
 function Login(props) {
   const [email, setEmail] = useState('');
@@ -6,39 +7,45 @@ function Login(props) {
 
   const handleLogin = () => {
     const { history } = props;
-    localStorage.setItem('user', JSON.stringify({email: email}))
-    localStorage.setItem('mealsToken', '1')
-    localStorage.setItem('cocktailsToken', '1')
-    history.push("/comidas")
-  }
+    localStorage.setItem('user', JSON.stringify({ email }));
+    localStorage.setItem('mealsToken', '1');
+    localStorage.setItem('cocktailsToken', '1');
+    history.push('/comidas');
+  };
 
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  const REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const MIN_PASSWORD_LENGTH = 6;
 
-  return(
+  return (
     <>
-    <input 
-      type="text"
-      value={ email }
-      onChange={ (e) => setEmail(e.target.value)}
-      data-testid="email-input"
-    />
-    <input 
-      type="password"
-      value={ password }
-      onChange={ (e) => setPassword(e.target.value)}
-      data-testid="password-input"
-    />
-    <button
-      type="button"
-      data-testid="login-submit-btn"
-      onClick={ handleLogin }
-      disabled={ !(regex.test(email) && password.length > 6)  }
-
-    >
-      Login
-    </button>
+      <input
+        type="text"
+        value={ email }
+        onChange={ (e) => setEmail(e.target.value) }
+        data-testid="email-input"
+      />
+      <input
+        type="password"
+        value={ password }
+        onChange={ (e) => setPassword(e.target.value) }
+        data-testid="password-input"
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        onClick={ handleLogin }
+        disabled={ !(REGEX.test(email) && password.length > MIN_PASSWORD_LENGTH) }
+      >
+        Login
+      </button>
     </>
-  )
+  );
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,13 +1,12 @@
-import React from "react";
-import { fireEvent, screen } from '@testing-library/react';
-import Login from "../pages/Login.js";
+import React from 'react';
+import { screen } from '@testing-library/react';
+import Login from '../pages/Login.js';
 
 describe('Testa componentes da tela de Login', () => {
-
   const EMAIL_INPUT_TEST_ID = 'email-input';
   const PASSWORD_INPUT_TEST_ID = 'password-input';
-  const VALID_EMAIL = 'alguem@email.com';
-  const VALID_PASSWORD = '123456';
+  // const VALID_EMAIL = 'alguem@email.com';
+  // const VALID_PASSWORD = '123456';
 
   test('se existem dois inputs', () => {
     render(<Login />);
@@ -16,5 +15,5 @@ describe('Testa componentes da tela de Login', () => {
 
     expect(email).toBeInTheDocument();
     expect(senha).toBeInTheDocument();
-  })
-})
+  });
+});
