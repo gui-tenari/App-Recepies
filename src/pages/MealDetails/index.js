614650d3d75022623df5bddd304c39d5f920b26b
Leonardo <leo_mc_inter@hotmail.com>
2021-09-23 17:34:49 -0300

    Requisito 33

--- src/pages/MealDetails/index.js
+++ src/pages/MealDetails/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const MealDetails = () => (
-  <div>
-    <h1>MealDetails</h1>
-  </div>
-);
+const MealDetails = (props) => {
+  const { match: { params: { id } } } = props;
+  return (
+    <div>
+      <img data-testid="recipe-photo"/>
+      <h1 data-testid="recipe-title">x</h1>
+      <button type="button" data-testid="share-btn">Compartilhar</button>
+      <button type="button" data-testid="favorite-btn">Favoritar</button>
+      <p data-testid="recipe-category">Categoria</p>
+      <div data-testid={`${id}-ingredient-name-and-measure`}>Ingredientes</div>
+      <p data-testid="instructions">Instruções</p>
+      <video data-testid="video">Video</video>
+      <div data-testid={`${id}-recomendation-card`}>Recomendações</div>
+      <button type="button" data-testid="start-recipe-btn">Iniciar Receita</button>
+    </div>
+  )
+};
 
 export default MealDetails;
