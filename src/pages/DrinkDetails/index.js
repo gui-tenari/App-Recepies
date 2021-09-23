b77049489f9acc25146755eb2f7b0ed5b5c20023
Leonardo <leo_mc_inter@hotmail.com>
2021-09-23 17:54:12 -0300

    Requisito 34

--- src/pages/DrinkDetails/index.js
+++ src/pages/DrinkDetails/index.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const DrinkDetails = () => (
-  <div>
-    <h1>Drink details</h1>
-  </div>
-);
+const DrinkDetails = (props) => {
+  const { match: { params: { id } } } = props;
+  const [drinks, setDrinks] = useState({});
+
+  useEffect(() => {
+    async function getDrink() {
+      const promiseDrinks = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const fetchedDrink = await promiseDrinks.json();
+      setDrinks(fetchedDrink.drinks[0]);
+    }
+    getDrink();
+  }, [])
+
+  return (
+    <div>
+      <img data-testid="recipe-photo" alt="drink"/>
+      <h1 data-testid="recipe-title">x</h1>
+      <button type="button" data-testid="share-btn">Compartilhar</button>
+      <button type="button" data-testid="favorite-btn">Favoritar</button>
+      <p data-testid="recipe-category">Categoria</p>
+      <div data-testid={`${id}-ingredient-name-and-measure`}>Ingredientes</div>
+      <p data-testid="instructions">Instruções</p>
+      <div data-testid={`${id}-recomendation-card`}>Recomendações</div>
+      <button type="button" data-testid="start-recipe-btn">Iniciar Receita</button>
+    </div>
+  )
+};
 
 export default DrinkDetails;
--- src/pages/MealDetails/index.js
+++ src/pages/MealDetails/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const MealDetails = (props) => {
   const { match: { params: { id } } } = props;
+  const [meals, setMeals] = useState({});
+
+  useEffect(() => {
+    async function getMeal() {
+      const promiseMeals = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const fetchedMeal = await promiseMeals.json();
+      setMeals(fetchedMeal.meals[0]);
+    }
+    getMeal();
+  }, [])
+
   return (
     <div>
-      <img data-testid="recipe-photo"/>
+      <img data-testid="recipe-photo" alt="meal"/>
       <h1 data-testid="recipe-title">x</h1>
       <button type="button" data-testid="share-btn">Compartilhar</button>
       <button type="button" data-testid="favorite-btn">Favoritar</button>
