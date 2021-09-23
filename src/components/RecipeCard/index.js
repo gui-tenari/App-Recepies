ab9fc7b85449f9b0ca721d23995bcf176398a20b
Leonardo <leo_mc_inter@hotmail.com>
2021-09-23 18:29:54 -0300

    Corrigindo erros lint

--- src/components/RecipeCard/index.js
+++ src/components/RecipeCard/index.js
@@ -5,9 +5,9 @@ import { Link, useLocation } from 'react-router-dom';
 const RecipeCard = ({ id, thumb, name, index }) => {
   const { pathname } = useLocation();
   return (
-    <div data-testid={`${index}-recipe-card`}>
-      <img data-testid={`${index}-card-img`} src={thumb} alt={name} />
-      <p data-testid={`${index}-card-name`}>{name}</p>
+    <div data-testid={ `${index}-recipe-card` }>
+      <img data-testid={ `${index}-card-img` } src={ thumb } alt={ name } />
+      <p data-testid={ `${index}-card-name` }>{ name }</p>
       <Link to={ `${pathname}/${id}` }>Mais detalhes</Link>
     </div>
   );
--- src/pages/DrinkDetails/index.js
+++ src/pages/DrinkDetails/index.js
@@ -11,21 +11,29 @@ const DrinkDetails = (props) => {
       setDrinks(fetchedDrink.drinks[0]);
     }
     getDrink();
-  }, [])
+  }, []);
 
   return (
     <div>
-      <img data-testid="recipe-photo" alt="drink"/>
-      <h1 data-testid="recipe-title">x</h1>
+      <img data-testid="recipe-photo" alt="drink" />
+      <h1 data-testid="recipe-title">{drinks.name}</h1>
       <button type="button" data-testid="share-btn">Compartilhar</button>
       <button type="button" data-testid="favorite-btn">Favoritar</button>
       <p data-testid="recipe-category">Categoria</p>
-      <div data-testid={`${id}-ingredient-name-and-measure`}>Ingredientes</div>
+      <div data-testid={ `${id}-ingredient-name-and-measure` }>Ingredientes</div>
       <p data-testid="instructions">Instruções</p>
-      <div data-testid={`${id}-recomendation-card`}>Recomendações</div>
+      <div data-testid={ `${id}-recomendation-card` }>Recomendações</div>
       <button type="button" data-testid="start-recipe-btn">Iniciar Receita</button>
     </div>
-  )
+  );
+};
+
+DrinkDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }).isRequired,
 };
 
 export default DrinkDetails;
