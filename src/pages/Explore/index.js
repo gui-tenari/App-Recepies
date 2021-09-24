8c353df29fcb01019a13b2b5b3639559cb76ad65
Dayvid Ruan <ruan12bbg@gmail.com>
2021-09-24 20:21:28 -0300

    Tela de explorar

--- src/pages/Explore/index.js
+++ src/pages/Explore/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-const Explore = () => (
-  <div />
-);
+const Explore = () => {
+  const history = useHistory();
+
+  function handleClickBebidas() {
+    history.push('/explorar/bebidas');
+  }
+
+  function handleClickComidas() {
+    history.push('/explorar/comidas');
+  }
+
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={ handleClickComidas }
+        data-testid="explore-food"
+      >
+        Explorar Comidas
+      </button>
+      <button
+        type="button"
+        onClick={ handleClickBebidas }
+        data-testid="explore-drinks"
+      >
+        Explorar Bebidas
+      </button>
+    </div>
+  );
+};
 
 export default Explore;
