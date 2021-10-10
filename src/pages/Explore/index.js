8e3b0b661b444cde77b147240abce0790033842e
Lucas Caribé <lucasmoc47@gmail.com>
2021-10-09 22:24:14 -0300

    Tela de explorar

--- src/pages/Explore/index.js
+++ src/pages/Explore/index.js
@@ -3,37 +3,32 @@ import { useHistory } from 'react-router-dom';
 
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import Button from '../../components/Button';
+
+import './style.css';
 
 const Explore = () => {
   const history = useHistory();
 
-  function handleClickBebidas() {
-    history.push('/explorar/bebidas');
-  }
-
-  function handleClickComidas() {
-    history.push('/explorar/comidas');
-  }
-
   return (
-    <div>
+    <>
       <Header title="Explorar" />
-      <button
-        type="button"
-        onClick={ handleClickComidas }
-        data-testid="explore-food"
-      >
-        Explorar Comidas
-      </button>
-      <button
-        type="button"
-        onClick={ handleClickBebidas }
-        data-testid="explore-drinks"
-      >
-        Explorar Bebidas
-      </button>
+      <div className="explore-content">
+        <div className="bg-meal">
+          <Button
+            text="Explorar Comidas"
+            onClick={ () => history.push('/explorar/comidas') }
+          />
+        </div>
+        <div className="bg-drink">
+          <Button
+            text="Explorar Bebidas"
+            onClick={ () => history.push('/explorar/bebidas') }
+          />
+        </div>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
